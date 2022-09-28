30c5571d737de59e051230ed0068a76c6478c3f1
pedroHen14 <pedrohenrique_silvasantos@yahoo.com>
2022-09-28 12:39:53 -0300

    Primeiro commit

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,30 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "../components/Button";
+import "../styles/sidebar.scss";
+
+interface SideBarProps {
+  genres: any[];
+  genreId: number;
+  fun: (id: number) => void;
+}
+
+export function SideBar(props: SideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {props.genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => props.fun(genre.id)}
+            selected={props.genreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
